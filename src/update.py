c55fe76d5f273ca786a4049855ba0ebd5563f5d1
Lande <laande.pro@gmail.com>
2025-06-22 12:24:13 +0200

    fix unarchived count & add type

--- src/update.py
+++ src/update.py
@@ -7,7 +7,7 @@ from src.utils import get_channel, time_format
 from src.config import BOT_GUILD_ID, STATUS_CHANNEL_ID
 
 
-async def forum_update(bot):
+async def forum_update(bot: discord.Client):
     time_start = time.perf_counter()
     server_ids = await db.get_servers()
     tasks = [process_server(server_id, bot) for server_id in server_ids]
@@ -18,7 +18,7 @@ async def forum_update(bot):
     
     total_time = time.perf_counter() - time_start
     res_msg = f"Forum update completed: {total_unarchived} thread{'s' if total_unarchived > 1 else ''} unarchived in {active_servers} server{'s' if active_servers > 1 else ''} ({time_format(total_time)})"
-    
+
     guild = bot.get_guild(BOT_GUILD_ID)
     channel = guild.get_channel(STATUS_CHANNEL_ID) if guild else None
     if channel:
@@ -27,7 +27,7 @@ async def forum_update(bot):
         print(res_msg)
 
 
-async def check_stil_exist(server_id, bot):
+async def check_still_exist(server_id: int, bot: discord.Client):
     server_ids = await db.get_servers()
     for server_id in server_ids:
         guild = bot.get_guild(server_id)
@@ -38,68 +38,70 @@ async def check_stil_exist(server_id, bot):
         channels = await db.get_channels(server_id)
         for channel_id in channels['category'] + channels['forum'] + channels['post']:
             channel = guild.get_channel(channel_id)
-
             if not channel:
                 await db.remove_channel(server_id, channel_id)
 
 
-async def process_server(server_id, bot):
+async def process_server(server_id: int, bot: discord.Client) -> int:
     already_check = set()
-    
-    await check_stil_exist(server_id, bot)
-    
+    unarchived_threads = 0
+
+    await check_still_exist(server_id, bot)
+
     posts = await db.get_posts_for_server(server_id)
     for post_id in posts:
-        await update_post(post_id, bot, already_check)
+        if post_id not in already_check:
+            already_check.add(post_id)
+            if await update_post(post_id, bot):
+                unarchived_threads += 1
 
     forums = await db.get_forums_for_server(server_id)
     for forum_id in forums:
         channel = bot.get_channel(forum_id)
         if channel:
-            await update_forum(channel, bot, already_check)
+            unarchived_threads += await update_forum(channel, bot, already_check)
 
     categories = await db.get_categories_for_server(server_id)
     for category_id in categories:
         channel = bot.get_channel(category_id)
         if channel:
-            await update_category(channel, bot, already_check)
-    
-    return len(already_check)
+            unarchived_threads += await update_category(channel, bot, already_check)
+
+    return unarchived_threads
 
 
-async def update_category(category: discord.CategoryChannel, bot, already_check: set):
+async def update_category(category: discord.CategoryChannel, bot: discord.Client, already_check: set) -> int:
+    count = 0
     for channel in category.channels:
         if isinstance(channel, discord.ForumChannel):
-            await update_forum(channel, bot, already_check)
+            count += await update_forum(channel, bot, already_check)
+    return count
 
 
-async def update_forum(forum: discord.ForumChannel, bot, already_check: set):
-    for thread in forum.threads:
-        await update_post(thread.id, bot, already_check)
-
-    try: 
+async def update_forum(forum: discord.ForumChannel, bot: discord.Client, already_check: set) -> int:
+    count = 0
+    try:
         async for thread in forum.archived_threads(limit=None):
-            await update_post(thread.id, bot, already_check)
+            if thread.id not in already_check:
+                already_check.add(thread.id)
+                if await update_post(thread.id, bot):
+                    count += 1
     except discord.errors.Forbidden:
         pass
+    return count
 
 
-async def update_post(thread_id: int, bot: discord.Client, already_check: set):
-    if thread_id in already_check:
-        return 0
-
-    already_check.add(thread_id)
-    thread = await bot.fetch_channel(thread_id)
-    if thread.archived:
-        try:
+async def update_post(thread_id: int, bot: discord.Client) -> bool:
+    try:
+        thread = await bot.fetch_channel(thread_id)
+        if thread.archived:
             await thread.edit(archived=False)
-            return 1
-        except discord.errors.Forbidden:
-            pass
-    return 0
+            return True
+    except discord.errors.NotFound:
+        return False
 
 
-async def get_monitored_posts(bot):
+async def get_monitored_posts(bot: discord.Client) -> set:
     post_set = set()
     channels = await db.list_all_channels()
     for item_id, category_type in channels:
